0153c68e43f4fb9a2b33ab833126bfbfb1031a37
Grape_L <grapelllar@gmail.com>
2022-06-06 16:25:57 +0800

    angularmaterialtest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,62 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Myc02NgforComponent } from './myc02-ngfor/myc02-ngfor.component';
-import { Myc03NgifComponent } from './myc03-ngif/myc03-ngif.component';
-import { Myc04NgifComponent } from './myc04-ngif/myc04-ngif.component';
-import { Myc06StyleComponent } from './myc06-style/myc06-style.component';
-import { Myc07SwitchComponent } from './myc07-switch/myc07-switch.component';
-import { Myc08NgmodelComponent } from './myc08-ngmodel/myc08-ngmodel.component';
-// 表单模块,内含ngModel
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialComponent } from './material/material.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatBadgeModule } from '@angular/material/badge'
+import { MatButtonModule } from '@angular/material/button'
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatStepperModule } from '@angular/material/stepper'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { FormsModule } from '@angular/forms';
-import { XuYaoQiangDiaoDirective } from './xu-yao-qiang-diao.directive';
-import { TodolistComponent } from './todolist/todolist.component';
-import { PipeComponent } from './pipe/pipe.component';
-import { SexPipe } from './pipe/sex.pipe';
-import { ZzmmPipeComponent } from './zzmm-pipe/zzmm-pipe.component';
-import { Zzmm } from './zzmm-pipe/zzmm.pip';
-import { Myc09Component } from './myc09/myc09.component';
+import { FormBuilder } from '@angular/forms'
+import { MatInputModule } from '@angular/material/input';
+import { TableComponent } from './table/table.component';
+// import { CdkTableModule } from "@angular/cdk/table";
+import {MatTableModule} from '@angular/material/table';
+import { StepperComponent } from './stepper/stepper.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmployeesComponent } from './employees/employees.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { Myc10Component } from './myc10/myc10.component';
-import { Class04Component } from './class04/class04.component';
-import { ParentBlogComponent } from './parent-blog/parent-blog.component';
-import { Child1ModifyComponent } from './child1-modify/child1-modify.component';
-import { Child2ModifyComponent } from './child2-modify/child2-modify.component';
-import { UserCenterComponent } from './user-center/user-center.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component'
 
 @NgModule({
   declarations: [//声明
-    AppComponent,
-    Myc02NgforComponent,
-    Myc03NgifComponent,
-    Myc04NgifComponent,
-    Myc06StyleComponent,
-    Myc07SwitchComponent,
-    Myc08NgmodelComponent,
-    XuYaoQiangDiaoDirective,
-    TodolistComponent,
-    PipeComponent,
-    SexPipe,
-    ZzmmPipeComponent, //管道声明
-    Zzmm,
-    Myc09Component,
-    Myc10Component,
-    Class04Component,
-    ParentBlogComponent,
-    Child1ModifyComponent,
-    Child2ModifyComponent,
-    UserCenterComponent,
-    ProductDetailComponent //HTTP对象声明
+    AppComponent, MaterialComponent, TableComponent, StepperComponent, EmployeesComponent,
+
   ],
   imports: [
-    BrowserModule, //浏览器模块,只要Angular用于Web项目就需要此模块,里面包含Common Module
+    BrowserModule,
+    MatSliderModule,
+    BrowserAnimationsModule, //浏览器模块,只要Angular用于Web项目就需要此模块,里面包含Common Module
+    MatBadgeModule,
+    MatButtonModule,
+    MatTreeModule,
+    MatIconModule,
+    MatDividerModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
     // AppRoutingModule
     FormsModule,
-    HttpClientModule,
+    // CdkTableModule
+    MatTableModule,
+    ReactiveFormsModule
+    // HttpClientModule,
   ],
-  providers: [],
+  providers: [FormBuilder],
   //启动项，引导组件，只有在这里导入才能引入index.html
   bootstrap: [AppComponent]
 })
